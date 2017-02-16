cae4a7ea99a0fe0354b7648bd77977da68830df8
Victor Quinn <mail@victorquinn.com>
2017-02-16 08:40:13 -0500

    Add CharFromPool, have Char use it under the hood

--- chance.go
+++ chance.go
@@ -5,9 +5,17 @@ import (
 	"strings"
 )
 
-// @todo make it so this can accept a pool and override the default
 func Char() string {
-	pool := strings.Split("abcdefghijklmnopqrstuvwxyz", "")
+	return CharFromPool("abcdefghijklmnopqrstuvwxyz")
+}
+
+// CharFromPool takes a string containing all the characters to add to the pool
+// and then selects one of them randomly
+func CharFromPool(p string) string {
+	pool := strings.Split(p, "")
+	if len(pool) < 1 {
+		panic("Cannot call CharFromPool with pool less than 1 character")
+	}
 	i := rand.Int()
-	return pool[i % 26]
+	return pool[i % len(pool)]
 }
--- chance_test.go
+++ chance_test.go
@@ -34,3 +34,39 @@ func TestChar(t *testing.T) {
 		})
 	})
 }
+
+func TestCharFromPool(t *testing.T) {
+	Convey("Existence", t, func() {
+		c := chance.CharFromPool("a")
+		So(c, ShouldNotBeNil)
+	})
+
+	Convey("Should throw error if called with empty pool", t, func() {
+		So(func() {
+			c := chance.CharFromPool("")
+			// This next line should never get hit but need to use c
+			// or Go complains
+			So(len(c), ShouldBeZeroValue)
+		}, ShouldPanic)
+	})
+
+	Convey("Generates random character", t, func() {
+		charRegex := regexp.MustCompile(`[a-e]`)
+		charMap := map[string]int{}
+		for i := 0; i < 500; i++ {
+			c := chance.CharFromPool("abcde")
+			So(charRegex.MatchString(c), ShouldBeTrue)
+			charMap[c] = charMap[c] + 1
+		}
+
+		Convey("Generated some of each character", func() {
+			So(len(charMap), ShouldEqual, 5)
+		})
+
+		Convey("And is relatively evenly distributed", func() {
+			for _, count := range charMap {
+				So(count, ShouldAlmostEqual, 100, 30)
+			}
+		})
+	})
+}
